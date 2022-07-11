0ff596d251212cdc644457c61d12f81b90ad3e80
Daniel Santos <daninsky12@gmail.com>
2022-07-11 15:43:08 -0300

    Add registration success page and login page

--- src/main/java/com/chessblast/AppController.java
+++ src/main/java/com/chessblast/AppController.java
@@ -1,11 +1,15 @@
 package com.chessblast;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AppController {
+    @Autowired
+    private UserRepository userRepository;
     @GetMapping("/index")
     public String homePage() {
         return "index";
@@ -15,5 +19,16 @@ public class AppController {
     public String registerPage(Model model) {
         model.addAttribute("user", new User());
         return "register";
+    }
+
+    @PostMapping("/process_registration")
+    public String processRegistration(User user) {
+        userRepository.save(user);
+        return "register_success";
+    }
+
+    @GetMapping("/login")
+    public String loginPage(User user) {
+        return "login"
     }
 }
