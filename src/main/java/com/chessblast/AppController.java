1fc5e3a73a5379a03a0da510b47858f6ec06b876
Daniel Santos <daninsky12@gmail.com>
2022-07-11 14:24:19 -0300

    Add first template page home page

---
+++ src/main/java/com/chessblast/AppController.java
@@ -0,0 +1,12 @@
+package com.chessblast;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+    @GetMapping("/index")
+    public String homePage() {
+        return "index";
+    }
+}
