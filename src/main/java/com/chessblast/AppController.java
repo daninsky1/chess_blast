08569bf01aa7506bf837064770fdf04f5cc78c4d
Daniel Santos <daninsky12@gmail.com>
2022-07-11 15:32:25 -0300

    Add register page
    Added register page and its route. But still
    missing the post submit.

--- src/main/java/com/chessblast/AppController.java
+++ src/main/java/com/chessblast/AppController.java
@@ -1,6 +1,7 @@
 package com.chessblast;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,5 +9,11 @@ public class AppController {
     @GetMapping("/index")
     public String homePage() {
         return "index";
+    }
+
+    @GetMapping("/register")
+    public String registerPage(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
     }
 }
