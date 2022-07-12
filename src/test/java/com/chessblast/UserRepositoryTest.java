9c2f632e6236b9ce3a52fc2275199a672571121d
Daniel Santos <daninsky12@gmail.com>
2022-07-12 17:32:07 -0300

    Fix UserRepository test

--- src/test/java/com/chessblast/UserRepositoryTest.java
+++ src/test/java/com/chessblast/UserRepositoryTest.java
@@ -2,20 +2,25 @@ package com.chessblast;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.checkerframework.checker.units.qual.A;
-import org.junit.jupiter.api.Test;
+import com.chessblast.model.User;
+import com.chessblast.model.UserRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 import java.util.List;
 
+//@SpringBootTest
+@RunWith(SpringRunner.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Rollback(false)
 public class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
@@ -23,14 +28,17 @@ public class UserRepositoryTest {
     @Autowired
     private TestEntityManager testEntityManager;
 
-    @Test
-    public void testCreateUser() {
-        userRepository.deleteAll();
-        List<User> users = new ArrayList<>();
+    List<User> users = new ArrayList<>();
+
+    public UserRepositoryTest() {
         users.add(new User("daninsky", "dan@dan.com", "1234"));
         users.add(new User("marina", "ma@dan.com", "1234"));
         users.add(new User("jose", "jose@dan.com", "1234"));
         users.add(new User("zilda", "zilda@dan.com", "1234"));
+    }
+
+    @Test
+    public void testSaveUser() {
 
         for (User user : users) {
             User savedUser = userRepository.save(user);
@@ -44,11 +52,9 @@ public class UserRepositoryTest {
     public void testFindUserByUsername() {
         String username = "daninsky";
 
-        userRepository.deleteAll();
-        userRepository.save(new User("daninsky", "dan@dan.com", "1234"));
-        userRepository.save(new User("marina", "ma@dan.com", "1234"));
-        userRepository.save(new User("jose", "jose@dan.com", "1234"));
-        userRepository.save(new User("zilda", "zilda@dan.com", "1234"));
+        for (User user : users) {
+            User savedUser = userRepository.save(user);
+        }
 
         User user = userRepository.findByUsername(username);
 
