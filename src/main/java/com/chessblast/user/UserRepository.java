bf8e50a9a37cde9048c4aba0fa7bd77d649de43e
Daniel Santos <daninsky12@gmail.com>
2022-07-14 12:06:39 -0300

    Update UserRepository super class
    CrudRepository was replaced by JpaRepository to add the jpa
    funcionalities.

--- src/main/java/com/chessblast/user/UserRepository.java
+++ src/main/java/com/chessblast/user/UserRepository.java
@@ -1,11 +1,8 @@
-package com.chessblast.model;
+package com.chessblast.user;
 
-import com.chessblast.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
