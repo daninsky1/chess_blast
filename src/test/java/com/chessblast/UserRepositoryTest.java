d8f4a5caaa2431d1666c1180fad9004ff0c80139
Daniel Santos <daninsky12@gmail.com>
2022-07-14 12:00:23 -0300

    Update UserRepositoryTest

--- src/test/java/com/chessblast/UserRepositoryTest.java
+++ src/test/java/com/chessblast/UserRepositoryTest.java
@@ -2,16 +2,14 @@ package com.chessblast;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.chessblast.model.User;
-import com.chessblast.model.UserRepository;
+import com.chessblast.user.User;
+import com.chessblast.user.UserRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
@@ -31,10 +29,10 @@ public class UserRepositoryTest {
     List<User> users = new ArrayList<>();
 
     public UserRepositoryTest() {
-        users.add(new User("daninsky", "dan@dan.com", "1234"));
-        users.add(new User("marina", "ma@dan.com", "1234"));
-        users.add(new User("jose", "jose@dan.com", "1234"));
-        users.add(new User("zilda", "zilda@dan.com", "1234"));
+        users.add(new User(1, "daninsky", "dan@dan.com", "1234"));
+        users.add(new User(2, "marina", "ma@dan.com", "1234"));
+        users.add(new User(3, "jose", "jose@dan.com", "1234"));
+        users.add(new User(4, "zilda", "zilda@dan.com", "1234"));
     }
 
     @Test
