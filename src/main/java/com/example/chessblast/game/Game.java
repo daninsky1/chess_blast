85b01baa96dadd4285435b4225a7612f8f281197
Daniel Santos <daninsky12@gmail.com>
2022-07-16 18:01:26 -0300

    Fix cascade type
    ALL to MERGE, ALL makes the reference entity to be created, and we don't
    want games creating new users when save is called, or in this case
    duplicated because users loaded are saved again as if they were new
    users breaking unique user constraints.

--- src/main/java/com/example/chessblast/game/Game.java
+++ src/main/java/com/example/chessblast/game/Game.java
@@ -1,6 +1,6 @@
 package com.example.chessblast.game;
 
-import com.example.chessblast.move.Moves;
+import com.example.chessblast.move.Move;
 import com.example.chessblast.user.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,14 +18,14 @@ public class Game {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne(cascade = CascadeType.MERGE)
     @JoinColumn(
         name = "white_player_id", nullable = false,
         foreignKey = @ForeignKey(name = "FK_white_player_game")
     )
-
     private User whitePlayer;
-    @ManyToOne(cascade = CascadeType.ALL)
+
+    @ManyToOne(cascade = CascadeType.MERGE)
     @JoinColumn(
         name = "black_player_id", nullable = false,
         foreignKey = @ForeignKey(name = "FK_black_player_game")
@@ -33,10 +33,10 @@ public class Game {
     private User blackPlayer;
 
     @Column(nullable = true)
-    private String result;
+    private GameResult result;
 
     @OneToMany(mappedBy = "game")
-    Set<Moves> moves;
+    Set<Move> moves;
 
     public Game(User whitePlayer, User blackPlayer) {
         this.whitePlayer = whitePlayer;
