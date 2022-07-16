a3a69a6ba9635d352ebe36aa17d46e4e45192afe
Daniel Santos <daninsky12@gmail.com>
2022-07-16 18:12:14 -0300

    Add querys to find games by users

--- src/main/java/com/example/chessblast/game/GameRepository.java
+++ src/main/java/com/example/chessblast/game/GameRepository.java
@@ -1,12 +1,16 @@
 package com.example.chessblast.game;
 
+import com.example.chessblast.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.function.Supplier;
 
 
 @Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
     Optional<Game> findById(long id);
+    Optional<Game> findByWhitePlayer(User user);
+    Optional<Game> findByBlackPlayer(User user);
 }
